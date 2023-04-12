3c52441d40a818e24a6730070d53efe7df7fb6d6
Gill Velleda Gonzales <gillvelleda@gmail.com>
2023-04-12 14:54:17 -0300

    refact: comentando o map de todos

--- src/aula06/src/App.js
+++ src/aula06/src/App.js
@@ -14,6 +14,7 @@ function App() {
   useEffect(() => {
     getDocs(collectionRef)
       .then(querySnap => {
+        //console.log(querySnap.docs.map(d=>d.data().todo))
         const docs = querySnap.docs
         if (!docs.length)
           throw Error("Empty data!")
