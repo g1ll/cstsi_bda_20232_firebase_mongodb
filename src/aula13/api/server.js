7b94d3b95ba3fb12f9f7d989f2f312c25dd97288
Gill Velleda Gonzales <gillvelleda@gmail.com>
2023-06-14 09:36:03 -0300

    feat: criação do arquivo server. Ponto de entrada do servidor para inicializar a  API Rest

---
+++ src/aula13/api/server.js
@@ -0,0 +1,14 @@
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+const port = 3000;
+
+const corsOptions = {
+  origin: 'http://localhost:3001',
+  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
+
+app.get('/', (req, res) => {
+  res.send({ express: `Servidor Express Iniciado na porta ${port}!` });
+});
