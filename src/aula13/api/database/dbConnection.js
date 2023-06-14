b0fc3205b2789475a8879d4eb373f23ac70d02dd
Gill Velleda Gonzales <gillvelleda@gmail.com>
2023-06-14 09:58:20 -0300

    refactor: remove comentarios

--- src/aula13/api/database/dbConnection.js
+++ src/aula13/api/database/dbConnection.js
@@ -2,16 +2,10 @@ import { MongoClient } from 'mongodb'
 
 const server = "localhost"
 
-//auth
-// const user = "gotuser"
-// const passwd = "123asd"
-// const uri = `mongodb://${user}:${passwd}@${server}:${port}/?authSource=${db}`;
-
 const port = "27017"
 const db = "loja"
 const uri = `mongodb://${server}:${port}/${db}`;
 
-// const client = new MongoClient(uri, { useUnifiedTopology: true });//auth
 const client = new MongoClient(uri);
 await client.connect()
 if (client.db('admin').command({ "ping": 1 }))
