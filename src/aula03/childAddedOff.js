e1ddf3debf046bdd855c8a4b5be6174100d580f8
Gill Velleda Gonzales <gillvelleda@gmail.com>
2023-08-24 11:03:31 -0300

    refactor: remocao do teste snapshot.exists

--- src/aula03/childAddedOff.js
+++ src/aula03/childAddedOff.js
@@ -1,22 +1,15 @@
 import { off, onChildAdded, ref } from "firebase/database";
 import db from "../libs/firebase/rtdb_connection.js"
 
-const node = "user"
-//CHILD ADDED
-let count =0;
-let refDB = ref(db,node);
-onChildAdded(refDB,(snapshot)=>{ //()=>{}
-  count++;
-  if(snapshot.exists()){
-      console.log(count)
-      console.table(snapshot.val())
-  }else{
-      console.log("Nó não encontrado")
-  }
-  
-  if(snapshot.key == 4){
-      console.log(snapshot.key)
-      console.log("Remove callback")
-      off(refDB,'child_added')
-  }
+const refNode = ref(db, "user");
+let count = 0;
+
+onChildAdded(refNode, (snapshot) => { 
+    console.log(++count)
+    console.table(snapshot.val())
+    if (snapshot.key == 4) {
+        console.log(snapshot.key)
+        console.log("Remove callback")
+        off(refNode, 'child_added')
+    }
 });
